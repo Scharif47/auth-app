02fbc033a1f9ef113f975a64971228477b30d7c7
Scharif47 <scharif47@outlook.de>
2023-08-19 21:44:52 +0200

    init commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+// Function for rendering the login page
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center mt-5">
+      <h1 className="text-white text-2xl">Login Page</h1>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+// Function for rendering the signup page
+export default function SignupPage() {
+  return (
+    <div className="flex justify-center mt-5">
+      <h1 className="text-white text-2xl">Signup Page</h1>
+    </div>
+  )
+}
