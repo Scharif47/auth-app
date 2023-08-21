459c824b33fc2375d2aa797fd4206ab4e78259dc
Scharif47 <scharif47@outlook.de>
2023-08-21 20:26:38 +0200

    userModel added

---
+++ src/models/userModel.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// Create a user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please enter a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyTokenExpiry: Date,
+});
+
+// Check if the user model already exists otherwise create a new one
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
